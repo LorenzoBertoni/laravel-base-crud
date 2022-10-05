2b77d420c950cbe3bc7ea7d2da1ca529835ec8ee
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-10-05 14:27:54 +0200

    add: funzione di validazione in ComicsController/update() + visualizzazione degli errori in edit.blade.php

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -13,71 +13,113 @@
             <label for="exampleInputEmail1" class="form-label">Titolo</label>
             <input 
             type="text"
-            class="form-control"
+            class="form-control @error('title') is-invalid @enderror"
             id="title"
             name="title"
-            value="{{$comic->title}}"
+            value="{{old('title', $comic->title)}}"
             >
+
+            @error('title')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <textarea 
             name="description"
             id="description"
-            class="form-control"
+            class="form-control @error('description') is-invalid @enderror"
             >
-                {{$comic->description}}
+                {{old('description', $comic->description)}}
             </textarea>
+
+            @error('description')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumbnail</label>
             <input 
             type="text" 
-            class="form-control"
+            class="form-control @error('thumb') is-invalid @enderror"
             id="thumb"
             name="thumb"
-            value="{{$comic->thumb}}"
+            value="{{old('thumb', $comic->thumb)}}"
             >
+
+            @error('thumb')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo:</label>
             <input 
             type="text" 
-            class="form-control"
+            class="form-control @error('price') is-invalid @enderror"
             id="price" 
             name="price"
-            value="{{$comic->price}}"
+            value="{{old('price', $comic->price)}}"
             >
+
+            @error('price')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Serie:</label>
             <input 
             type="text" 
-            class="form-control"
+            class="form-control @error('series') is-invalid @enderror"
             id="series"
             name="series"
-            value="{{$comic->series}}"
+            value="{{old('series', $comic->series)}}"
             >
+
+            @error('series')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita:</label>
             <input 
             type="date"
-            class="form-control" 
+            class="form-control @error('sale_date') is-invalid @enderror" 
             id="sale_date" 
             name="sale_date"
-            value="{{$comic->sale_date}}"
+            value="{{old('sale_date', $comic->sale_date)}}"
             >
+
+            @error('sale_date')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Genere:</label>
             <input 
             type="type" 
-            class="form-control" 
+            class="form-control @error('type') is-invalid @enderror" 
             id="type" 
             name="type" 
-            value="{{$comic->type}}"
+            value="{{old('type', $comic->type)}}"
             >
+
+            @error('type')
+                <div class='invalid-feedback'>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         
         <button type="submit" class="btn btn-primary">Modifica</button>
